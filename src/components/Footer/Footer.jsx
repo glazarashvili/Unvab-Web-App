7ef23a93e2c2bef4ffe9dee6517a473020930aeb
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-06-25 14:07:40 +0400

    added footer page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,13 @@ import './Footer.scss'
 
 const Footer = () => {
   return (
-    <footer>
-      <img src={twitterSvg} alt='fb logo' />
-      <img src={fbSvg} alt='fb logo' />
-      <img src={dribbleSvg} alt='dribble logo' />
-      <img src={igSvg} alt='ig logo' />
+    <footer className='footer-container'>
+      <div>
+        <img src={twitterSvg} alt='fb logo' />
+        <img src={fbSvg} alt='fb logo' />
+        <img src={dribbleSvg} alt='dribble logo' />
+        <img src={igSvg} alt='ig logo' />
+      </div>
       <p>2016-2017 © Design by Unvab. Code by nK</p>
     </footer>
   )
